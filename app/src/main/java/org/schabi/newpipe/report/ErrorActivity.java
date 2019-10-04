a086a3633e171d2c660ab6bbb0550a2f94156fcf
TobiGr <tobigr@users.noreply.github.com>
2019-10-05 00:20:28 +0200

    Add handling for passed documents by extractor
    Add buttons to open NewPipe GitHub issues to ErrorActivity

--- app/src/main/java/org/schabi/newpipe/report/ErrorActivity.java
+++ app/src/main/java/org/schabi/newpipe/report/ErrorActivity.java
@@ -2,6 +2,8 @@ package org.schabi.newpipe.report;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
@@ -18,15 +20,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.*;
 
 import org.acra.ReportField;
 import org.acra.collector.CrashReportData;
@@ -36,15 +37,13 @@ import org.schabi.newpipe.ActivityCommunicator;
 import org.schabi.newpipe.BuildConfig;
 import org.schabi.newpipe.MainActivity;
 import org.schabi.newpipe.R;
+import org.schabi.newpipe.extractor.exceptions.ParsedDocumentInfo;
 import org.schabi.newpipe.util.ThemeHelper;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.TimeZone;
-import java.util.Vector;
+import java.util.*;
 
 /*
  * Created by Christian Schabesberger on 24.10.15.
@@ -72,11 +71,16 @@ public class ErrorActivity extends AppCompatActivity {
     // BUNDLE TAGS
     public static final String ERROR_INFO = "error_info";
     public static final String ERROR_LIST = "error_list";
+    public static final String ERROR_DOCUMENT_LIST = "error_document_list";
 
     public static final String ERROR_EMAIL_ADDRESS = "crashreport@newpipe.schabi.org";
     public static final String ERROR_EMAIL_SUBJECT = "Exception in NewPipe " + BuildConfig.VERSION_NAME;
+
+    public static final String ERROR_GITHUB_ISSUE_URL = "https://github.com/TeamNewPipe/NewPipe/issues";
+
     private String[] errorList;
     private ErrorInfo errorInfo;
+    private String[] parsedDocumentList;
     private Class returnActivity;
     private String currentTimeStamp;
     private EditText userCommentBox;
@@ -86,24 +90,40 @@ public class ErrorActivity extends AppCompatActivity {
                 ErrorInfo.make(UserAction.UI_ERROR, "none", "", R.string.app_ui_crash));
     }
 
-    public static void reportError(final Context context, final List<Throwable> el,
+    public static void reportError(final Context context, final List<Throwable> el, final List<String> docs,
                                    final Class returnActivity, View rootView, final ErrorInfo errorInfo) {
         if (rootView != null) {
             Snackbar.make(rootView, R.string.error_snackbar_message, 3 * 1000)
                     .setActionTextColor(Color.YELLOW)
                     .setAction(R.string.error_snackbar_action, v ->
-                            startErrorActivity(returnActivity, context, errorInfo, el)).show();
+                            startErrorActivity(returnActivity, context, errorInfo, el, docs)).show();
         } else {
-            startErrorActivity(returnActivity, context, errorInfo, el);
+            startErrorActivity(returnActivity, context, errorInfo, el, docs);
         }
     }
 
-    private static void startErrorActivity(Class returnActivity, Context context, ErrorInfo errorInfo, List<Throwable> el) {
+    public static void reportError(final Context context, final List<Throwable> el,
+                                   final Class returnActivity, View rootView, final ErrorInfo errorInfo) {
+        List<String> docs = new ArrayList<>();
+        for (Throwable e : el) {
+            if (e instanceof ParsedDocumentInfo) {
+                docs.add(((ParsedDocumentInfo) e).getDocument());
+            } else {
+                docs.add(null);
+            }
+        }
+
+        reportError(context, el, docs, returnActivity, rootView, errorInfo);
+    }
+
+    private static void startErrorActivity(Class returnActivity, Context context, ErrorInfo errorInfo,
+                                           List<Throwable> el, List<String> docs) {
         ActivityCommunicator ac = ActivityCommunicator.getCommunicator();
         ac.returnActivity = returnActivity;
         Intent intent = new Intent(context, ErrorActivity.class);
         intent.putExtra(ERROR_INFO, errorInfo);
         intent.putExtra(ERROR_LIST, elToSl(el));
+        intent.putExtra(ERROR_DOCUMENT_LIST, docs.toArray(new String[0]));
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(intent);
     }
@@ -169,6 +189,38 @@ public class ErrorActivity extends AppCompatActivity {
         return out;
     }
 
+    private void openPrivacyPolicyDialog(Context context, String action, boolean appendDocument) {
+        new AlertDialog.Builder(context)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle(R.string.privacy_policy_title)
+                .setMessage(R.string.start_accept_privacy_policy)
+                .setCancelable(false)
+                .setNeutralButton(R.string.read_privacy_policy, (dialog, which) -> {
+                    Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse(context.getString(R.string.privacy_policy_url))
+                    );
+                    context.startActivity(webIntent);
+                })
+                .setPositiveButton(R.string.accept, (dialog, which) -> {
+                    if (action.equals("EMAIL")) { // send on email
+                        Intent i = new Intent(Intent.ACTION_SENDTO);
+                        i.setData(Uri.parse("mailto:" + ERROR_EMAIL_ADDRESS))
+                                .putExtra(Intent.EXTRA_SUBJECT, ERROR_EMAIL_SUBJECT)
+                                .putExtra(Intent.EXTRA_TEXT, buildJson(appendDocument));
+                        startActivity(Intent.createChooser(i, "Send Email"));
+                    } else if (action.equals("GITHUB")) { // open the NewPipe issue page on GitHub
+                        Intent i = new Intent(Intent.ACTION_VIEW);
+                        i.setData(Uri.parse(ERROR_GITHUB_ISSUE_URL));
+                        startActivity(Intent.createChooser(i, "Report issue on GitHub"));
+                    }
+
+                })
+                .setNegativeButton(R.string.decline, (dialog, which) -> {
+                    // do nothing
+                })
+                .show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -187,7 +239,12 @@ public class ErrorActivity extends AppCompatActivity {
             actionBar.setDisplayShowTitleEnabled(true);
         }
 
-        Button reportButton = findViewById(R.id.errorReportButton);
+        Button reportEmailButton = findViewById(R.id.errorReportEmailButton);
+        Button copyButton = findViewById(R.id.errorReportCopyButton);
+        Button reportGithubButton = findViewById(R.id.errorReportGitHubButton);
+        LinearLayout includeDocumentsSwitchContainer = findViewById(R.id.includeDocumentsSwitchContainer);
+        SwitchCompat includeDocumentsSwitch = findViewById(R.id.includeDocumentsSwitch);
+
         userCommentBox = findViewById(R.id.errorCommentBox);
         TextView errorView = findViewById(R.id.errorView);
         TextView infoView = findViewById(R.id.errorInfosView);
@@ -197,37 +254,44 @@ public class ErrorActivity extends AppCompatActivity {
         returnActivity = ac.returnActivity;
         errorInfo = intent.getParcelableExtra(ERROR_INFO);
         errorList = intent.getStringArrayExtra(ERROR_LIST);
+        parsedDocumentList = intent.getStringArrayExtra(ERROR_DOCUMENT_LIST);
+
+        // do not show documentsSwitch when no documents were passed to ErrorActivity
+        if (parsedDocumentList == null || parsedDocumentList.length == 0) {
+            includeDocumentsSwitchContainer.setVisibility(View.GONE);
+        } else {
+            boolean documentListIsEmpty = true;
+            for (int i = 0; i < parsedDocumentList.length && documentListIsEmpty; i++) {
+                if (parsedDocumentList[i] != null && parsedDocumentList[i].length() > 0)
+                    documentListIsEmpty = false;
+            }
+            if (documentListIsEmpty) includeDocumentsSwitchContainer.setVisibility(View.GONE);
+        }
+
 
         // important add guru meditation
-        addGuruMeditaion();
+        addGuruMeditation();
         currentTimeStamp = getCurrentTimeStamp();
 
-        reportButton.setOnClickListener((View v) -> {
-            Context context = this;
-            new AlertDialog.Builder(context)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setTitle(R.string.privacy_policy_title)
-                    .setMessage(R.string.start_accept_privacy_policy)
-                    .setCancelable(false)
-                    .setNeutralButton(R.string.read_privacy_policy, (dialog, which) -> {
-                        Intent webIntent = new Intent(Intent.ACTION_VIEW,
-                                Uri.parse(context.getString(R.string.privacy_policy_url))
-                        );
-                        context.startActivity(webIntent);
-                    })
-                    .setPositiveButton(R.string.accept, (dialog, which) -> {
-                        Intent i = new Intent(Intent.ACTION_SENDTO);
-                        i.setData(Uri.parse("mailto:" + ERROR_EMAIL_ADDRESS))
-                                .putExtra(Intent.EXTRA_SUBJECT, ERROR_EMAIL_SUBJECT)
-                                .putExtra(Intent.EXTRA_TEXT, buildJson());
+        reportEmailButton.setOnClickListener((View v) -> {
+            openPrivacyPolicyDialog(this, "EMAIL", false);
+        });
 
-                        startActivity(Intent.createChooser(i, "Send Email"));
-                    })
-                    .setNegativeButton(R.string.decline, (dialog, which) -> {
-                        // do nothing
-                    })
-                    .show();
+        copyButton.setOnClickListener((View v) -> {
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText(
+                    "NewPipe error report",
+                    buildMarkdown(includeDocumentsSwitch.isChecked()));
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(this, R.string.msg_copied, Toast.LENGTH_SHORT).show();
+        });
 
+        reportGithubButton.setOnClickListener((View v) -> {
+            openPrivacyPolicyDialog(this, "GITHUB", includeDocumentsSwitch.isChecked());
+        });
+
+        includeDocumentsSwitchContainer.setOnClickListener((View v) -> {
+            includeDocumentsSwitch.toggle();
         });
 
         // normal bugreport
@@ -276,11 +340,13 @@ public class ErrorActivity extends AppCompatActivity {
     private String formErrorText(String[] el) {
         StringBuilder text = new StringBuilder();
         if (el != null) {
-            for (String e : el) {
-                text.append("-------------------------------------\n").append(e);
+            for (int i = 0; i < el.length; i++) {
+                text.append(el[i]);
+                if (i != el.length - 1) {
+                    text.append("-------------------------------------\n");
+                }
             }
         }
-        text.append("-------------------------------------");
         return text.toString();
     }
 
@@ -323,7 +389,8 @@ public class ErrorActivity extends AppCompatActivity {
 
         text += getUserActionString(info.userAction)
                 + "\n" + info.request
-                + "\n" + getContentLangString()
+                + "\n" + getContentCountryString()
+                + "\n" + getContentLanguageString()
                 + "\n" + info.serviceName
                 + "\n" + currentTimeStamp
                 + "\n" + getPackageName()
@@ -334,12 +401,17 @@ public class ErrorActivity extends AppCompatActivity {
     }
 
     private String buildJson() {
+        return buildJson(false);
+    }
+
+    private String buildJson(boolean appendDocuments) {
         JSONObject errorObject = new JSONObject();
 
         try {
             errorObject.put("user_action", getUserActionString(errorInfo.userAction))
                     .put("request", errorInfo.request)
-                    .put("content_language", getContentLangString())
+                    .put("content_country", getContentCountryString())
+                    .put("content_language", getContentCountryString())
                     .put("service", errorInfo.serviceName)
                     .put("package", getPackageName())
                     .put("version", BuildConfig.VERSION_NAME)
@@ -353,7 +425,14 @@ public class ErrorActivity extends AppCompatActivity {
                 }
             }
 
-            errorObject.put("exceptions", exceptionArray);
+            JSONArray exceptions = new JSONArray();
+            for (int i = 0; i < exceptionArray.length(); i++) {
+                JSONObject exception = new JSONObject();
+                exception.put("stacktrace", errorList[i]);
+                exception.put("document", !appendDocuments || parsedDocumentList[i] == null ? "" : parsedDocumentList[i]);
+                exceptions.put(exception);
+            }
+            errorObject.put("exceptions", exceptions);
             errorObject.put("user_comment", userCommentBox.getText().toString());
 
             return errorObject.toString(3);
@@ -365,6 +444,56 @@ public class ErrorActivity extends AppCompatActivity {
         return "";
     }
 
+    private String buildMarkdown(boolean appendDocuments) {
+        StringBuilder htmlErrorReport = new StringBuilder();
+        // basic error info
+        htmlErrorReport
+                .append("## Exception")
+                .append("\n* __User Action:__ ").append(getUserActionString(errorInfo.userAction))
+                .append("\n* __Request:__ ").append(errorInfo.request)
+                .append("\n* __Content Country:__ ").append(getContentCountryString())
+                .append("\n* __Content Language:__ ").append(getContentLanguageString())
+                .append("\n* __Service:__ ").append(errorInfo.serviceName)
+                .append("\n* __Version:__ ").append(BuildConfig.VERSION_NAME)
+                .append("\n* __OS:__ ").append(getOsString())
+                .append(userCommentBox.getText().toString()).append("\n");
+
+        // collapse all logs to a single paragraph when there are more than one to keep GitHub issue clean
+        if (errorList.length > 1) {
+            htmlErrorReport
+                    .append("<details><summary><b>Exceptions (")
+                    .append(errorList.length + 1)
+                    .append(")</b></summary><p>\n");
+        }
+
+        // most importantly, don't forget the crash logs ^^
+        for (int i = 0; i < errorList.length; i++) {
+            boolean appendDocument = appendDocuments && parsedDocumentList[i] != null
+                    && parsedDocumentList[i].length() > 0;
+            htmlErrorReport
+                    .append("<details><summary><b>Crash log ").append(i + 1).append("</b>")
+                    .append((appendDocument ? " with parsed document" : ""))
+                    .append("</summary><p>\n")
+                    .append("\n```\n").append(errorList[i]).append("\n```\n");
+
+            if (appendDocument) {
+                htmlErrorReport
+                        .append("\n```HTML\n")
+                        .append(parsedDocumentList[i])
+                        .append("\n```\n");
+            }
+            htmlErrorReport.append("</details>\n");
+        }
+
+        // make sure to close everything
+        if (errorList.length > 1) {
+            htmlErrorReport.append("</p></details>\n");
+        }
+        htmlErrorReport.append("<hr>\n");
+
+        return htmlErrorReport.toString();
+    }
+
     private String getUserActionString(UserAction userAction) {
         if (userAction == null) {
             return "Your description is in another castle.";
@@ -373,9 +502,15 @@ public class ErrorActivity extends AppCompatActivity {
         }
     }
 
-    private String getContentLangString() {
+    private String getContentCountryString() {
         return PreferenceManager.getDefaultSharedPreferences(this)
                 .getString(this.getString(R.string.content_country_key), "none");
+    }
+
+
+    private String getContentLanguageString() {
+        return PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(this.getString(R.string.content_language_key), "none");
     }
 
     private String getOsString() {
@@ -386,7 +521,7 @@ public class ErrorActivity extends AppCompatActivity {
                 + " - " + Integer.toString(Build.VERSION.SDK_INT);
     }
 
-    private void addGuruMeditaion() {
+    private void addGuruMeditation() {
         //just an easter egg
         TextView sorryView = findViewById(R.id.errorSorryView);
         String text = sorryView.getText().toString();
