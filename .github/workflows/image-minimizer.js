dd6de59dfe05ca3af81417591ac05e87c99ca107
TobiGr <tobigr@users.noreply.github.com>
2023-07-15 02:01:11 +0200

    Minimize images in PR descriptions

--- .github/workflows/image-minimizer.js
+++ .github/workflows/image-minimizer.js
@@ -17,8 +17,11 @@ module.exports = async ({github, context}) => {
         initialBody = context.payload.comment.body;
     } else if (context.eventName == 'issues') {
         initialBody = context.payload.issue.body;
+    } else if (context.eventName == 'pull_request') {
+        console.log(context.payload);
+        initialBody = context.payload.pull_request.body;
     } else {
-        console.log('Aborting: No body found');
+        console.log('Aborting: No body found', context.eventName);
         return;
     }
     console.log(`Found body: \n${initialBody}\n`);
@@ -59,7 +62,7 @@ module.exports = async ({github, context}) => {
 
     // Update the corresponding element
     if (context.eventName == 'issue_comment') {
-        console.log('Updating comment with id', context.payload.comment.id);
+        console.log('Updating issue comment with id', context.payload.comment.id);
         await github.rest.issues.updateComment({
             comment_id: context.payload.comment.id,
             owner: context.repo.owner,
@@ -70,6 +73,14 @@ module.exports = async ({github, context}) => {
         console.log('Updating issue', context.payload.issue.number);
         await github.rest.issues.update({
             issue_number: context.payload.issue.number,
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            body: newBody
+        });
+    } else if (context.eventName == 'pull_request') {
+        console.log('Updating pull request', context.payload.pull_request.number);
+        await github.rest.pulls.update({
+            pull_number: context.payload.pull_request.number,
             owner: context.repo.owner,
             repo: context.repo.repo,
             body: newBody
